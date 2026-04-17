306c3027a15cff68275e082ac498931dc03f4fc7
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2026-04-17 09:32:58 +0200

    try to solve deployment bugs

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,15 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    dynamicIO: true,
+    // dynamicIO: true,
+    cacheComponents: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 }
 
 export default nextConfig
